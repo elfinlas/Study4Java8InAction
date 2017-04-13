884f6f53b3f26e2b5c4df3a873300e47f25f7716
Elfinlas <wisseraph@naver.com>
2017-04-13 18:05:38 +0900

    [2017.04.13]
    * 예제 추가

---
+++ Study4Java8InAction/src/com/study/res/Apple.java
@@ -0,0 +1,24 @@
+package com.study.res;
+
+public class Apple {
+	private Integer weight;
+	private String color;
+	
+	public Apple(Integer weight, String color) {
+		this.weight = weight;
+		this.color = color;
+	}
+	
+	public Integer getWeight() {
+		return weight;
+	}
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
